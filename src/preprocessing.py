d3322e6e990277ab9e9201c7ca30e105d45c2638
Daeyeon Kim <dkim20@leomail.tamuc.edu>
2025-09-14 15:47:58 -0500

    Fixed errors - Changed from using an absolute path to a relative path using os

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,8 +1,16 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+import os
 
-def load_data(path="\\data\final_game_logs.csv"):
+def load_data(path=None):
+    if path is None:
+        base_dir = os.path.dirname(__file__)
+        path = os.path.join(base_dir, "..", "data", "final_game_logs.csv")
+    abs_path = os.path.abspath(path)
+    
+    if not os.path.exists(abs_path):
+        raise FileNotFoundError(f"Data File not found")
     df = pd.read_csv(path)
     return df
 
@@ -23,8 +31,10 @@ def preprocess(df):
     
     return X_scaled, y
 
-df = load_data()
-X, y = preprocess(df)
+if __name__ == "__main__":
+    df = load_data()
+    X, y = preprocess(df)
 
-# Train Test Split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False) # Disable shuffling
+    # Train Test Split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False) # Disable shuffling
+    print(X_train.shape, X_test.shape)
